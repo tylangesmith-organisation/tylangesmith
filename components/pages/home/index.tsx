300183580c2e13fa5493071b09cef13149ec2c84
Ty Lange-Smith <tylangesmith@gmail.com>
2021-05-20 14:25:42 +1000

    Content updates

--- components/pages/home/index.tsx
+++ components/pages/home/index.tsx
@@ -4,6 +4,7 @@ import Container from "../../ui/container/container";
 import Emoji from "../../ui/emoji/emoji";
 import Heading from "./heading";
 import SubHeading from "./subHeading";
+import Paragraph from "./paragraph";
 import Image from "../../ui/image/image";
 import Label from "../../ui/image/label";
 
@@ -29,7 +30,7 @@ const Component = (): JSX.Element => (
     <Container padding={false}>
       <div className="my-8 md:my-28">
         <Image
-          className="sm:rounded"
+          className="sm:rounded shadow-md"
           lowQualitySource="/hero-low-res.png"
           highQualitySource="/hero-high-res.png"
           alt="Crescent Head Beach"
@@ -37,6 +38,24 @@ const Component = (): JSX.Element => (
         <Label>
           A rare sight of me doing something other than software stuff
         </Label>
+      </div>
+    </Container>
+
+    <Container>
+      <div className="my-10 md:my-28">
+        <Paragraph>
+          Coming from a strong DevOps background I understand the importance of
+          cultural DevOps adoption. I have a proven history of helping companies
+          drive DevOps culture, engaging with various technical and
+          non-technical stakeholders across a range of different team
+          topologies.
+        </Paragraph>
+        <Paragraph>
+          I have seen firsthand the benefits companies gain from this adoption.
+          Whether it be the ability to pivot and experiment with best in class
+          tooling or be the first to market delivering value with velocity and
+          confidence.
+        </Paragraph>
       </div>
     </Container>
   </StandardLayout>
