8ee614afd00c9767d54c3668d401b52c3a7beb47
Ty Lange-Smith <tylangesmith@gmail.com>
2021-05-20 14:28:05 +1000

    Update paragraph component responsiveness

--- components/pages/home/paragraph.tsx
+++ components/pages/home/paragraph.tsx
@@ -7,7 +7,7 @@ export interface Props {
 const Component = (props: Props): JSX.Element => {
   const { children } = props;
   return (
-    <p className="font-medium text-xl md:text-2xl my-3 text-gray-600">
+    <p className="font-medium text-md sm:text-xl md:text-2xl my-3 text-gray-600">
       {children}
     </p>
   );
