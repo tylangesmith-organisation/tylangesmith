1323c2fb9610210bd4070e1bf870be940b9c6f84
Ty Lange-Smith <tylangesmith@gmail.com>
2021-05-28 15:26:06 +1000

    Ok not just yet

--- .github/deploy/scripts/lint_test_build.sh
+++ .github/deploy/scripts/lint_test_build.sh
@@ -9,11 +9,5 @@ npm i
 echo "--- 🚀 Run next build..."
 npm run build
 
-echo "--- 🚀 Run linter..."
-npm run lint
-
-echo "--- 🚀 Run tests..."
-npm run test
-
 # For now we only want the static bundle
 zip -r build.zip out
