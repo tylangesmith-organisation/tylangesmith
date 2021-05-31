359c221d6fd24f47f36238576efa9672aba06176
Ty Lange-Smith <tylangesmith@gmail.com>
2021-05-31 13:15:12 +1000

    Think we also need that transition there too

--- components/pages/hireMe/copyToClipboard.tsx
+++ components/pages/hireMe/copyToClipboard.tsx
@@ -31,7 +31,7 @@ const Component = (props: Props) => {
           </svg>
         </div>
       </button>
-      <div className={`flex items-center bg-black text-white text-sm rounded px-2 py-1 mx-2 transition ${popoverVisible ? 'opacity-100' : 'opacity-0 ease-out duration-150'}`}>
+      <div className={`flex items-center bg-black text-white text-sm rounded px-2 py-1 mx-2 ${popoverVisible ? 'opacity-100' : 'opacity-0 transition ease-out duration-150'}`}>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 font-bold mr-1 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M5 13l4 4L19 7" />
         </svg>
