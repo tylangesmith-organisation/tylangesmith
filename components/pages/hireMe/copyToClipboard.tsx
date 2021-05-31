42859d2d1fced89a14a381a95eb16249930ec21e
Ty Lange-Smith <tylangesmith@gmail.com>
2021-05-31 12:56:17 +1000

    Ok see if that works better

--- components/pages/hireMe/copyToClipboard.tsx
+++ components/pages/hireMe/copyToClipboard.tsx
@@ -19,19 +19,29 @@ const Component = (props: Props) => {
   }
 
   return (
-    <button
+    <div className="flex items-center">
+      <button
       className="text-lg text-gray-900 focus:outline-none"
       onClick={() => handleOnClick(children)}
-    >
-      {children}
-      <span
-        className={` bg-black text-white text-sm rounded px-3 py-1 mx-2 transition ease-out duration-150 ${
-          popoverVisible ? 'visible opacity-100' : 'invisible opacity-0'
-        }`}
       >
-        Copied!
-      </span>
-    </button>
+        <div className="flex items-center">
+          {children}
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 text-gray-900 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
+          </svg>
+        </div>
+      </button>
+      {
+        popoverVisible && (
+          <div className="flex items-center bg-black text-white text-sm rounded px-2 py-1 mx-2">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 font-bold mr-1 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M5 13l4 4L19 7" />
+            </svg>
+            Copied!
+          </div>
+        )
+      }
+    </div>
   )
 }
 
