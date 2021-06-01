83e78688ca3c4717b1e90798e8a746d4a648bb12
Ty Lange-Smith <tylangesmith@gmail.com>
2021-06-02 08:20:13 +1000

    Try with the new shiny execution role

--- .github/deploy/src/helpers/lambda.ts
+++ .github/deploy/src/helpers/lambda.ts
@@ -1,5 +1,6 @@
 import { Stack } from '@aws-cdk/core'
 import { Function, Runtime, AssetCode, Version } from '@aws-cdk/aws-lambda'
+import { Role, PolicyStatement, ServicePrincipal, CompositePrincipal, Effect } from '@aws-cdk/aws-iam'
 
 export interface CreateFunctionVersionProps {
   scope: Stack;
@@ -10,10 +11,33 @@ export interface CreateFunctionVersionProps {
 export const createFunctionVersion = (props: CreateFunctionVersionProps): Version => {
   const { scope, name, handler } = props
 
+  const executionRole = new Role(scope, `${name}-execution-role`, {
+    assumedBy: new CompositePrincipal(
+      new ServicePrincipal('lambda.amazonaws.com'),
+      new ServicePrincipal('edgelambda.amazonaws.com')
+    )
+  })
+
+  executionRole.addToPolicy(new PolicyStatement({
+    effect: Effect.ALLOW,
+    actions: ['sts:AssumeRole']
+  }))
+
+  executionRole.addToPolicy(new PolicyStatement({
+    effect: Effect.ALLOW,
+    actions: [
+      'logs:CreateLogStream',
+      'logs:PutLogEvents',
+      'logs:CreateLogGroup'
+    ],
+    resources: ['*']
+  }))
+
   const lambda = new Function(scope, `${name}-function`, {
     runtime: Runtime.NODEJS_14_X,
     code: new AssetCode('./src/functions'),
-    handler
+    handler,
+    role: executionRole
   })
 
   return new Version(scope, `${name}-function-version`, {
