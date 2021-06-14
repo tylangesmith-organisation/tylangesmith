8c26aafba15f6ac1fe9072d99e797bbb0ead182e
Ty Lange-Smith <tylangesmith@gmail.com>
2021-06-14 13:35:27 +1000

    Better section margins

--- components/ui/section/section.tsx
+++ components/ui/section/section.tsx
@@ -10,7 +10,7 @@ const Component = (props: Props): JSX.Element => {
   const { children, pose = false, padding = true } = props
   return (
     <Container padding={padding}>
-      <div className={`my-8 md:my-28 ${pose ? 'max-w-prose' : ''}`}>
+      <div className={`my-8 md:my-20 ${pose ? 'max-w-prose' : ''}`}>
         {children}
       </div>
     </Container>
