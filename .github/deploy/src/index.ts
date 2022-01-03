5d943b85ac3ef4b6845943090dfed1d949c574fd
Ty Lange-Smith <tylangesmith@gmail.com>
2022-01-03 20:10:31 +1100

    Update AWS CDK package versions

--- .github/deploy/src/index.ts
+++ .github/deploy/src/index.ts
@@ -2,9 +2,9 @@ import { App } from '@aws-cdk/core'
 import { getSubDomainName, getUrl } from './helpers/helper'
 import Stack from './stack'
 
+// Ensure we have the required environment variables
 if (!process.env.BRANCH_NAME) throw Error('A BRANCH_NAME must be provided...')
 if (!process.env.DOMAIN_NAME) throw Error('A DOMAIN_NAME must be provided...')
-
 const branchName = process.env.BRANCH_NAME
 
 // e.g. tylangesmith.com
@@ -18,10 +18,5 @@ new Stack(app, {
   stackName: `tylangesmith-${branchName}`,
   url,
   domainName,
-  subDomainName,
-  env: {
-    // Need to pass these in to lookup the route53 hostedZone
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION
-  }
+  subDomainName
 })
