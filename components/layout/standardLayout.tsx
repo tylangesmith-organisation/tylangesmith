37a6db7e944063344054d9fb183bf0da3b4a4f7a
Ty Lange-Smith <tylangesmith@gmail.com>
2022-02-09 08:25:08 +1100

    Try to add favicon

--- components/layout/standardLayout.tsx
+++ components/layout/standardLayout.tsx
@@ -19,6 +19,14 @@ const Component = (props: Props) => {
       <NextSeo
         title={title}
         description={description}
+        additionalLinkTags={
+          [
+            {
+              rel: 'icon',
+              href: '"data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨🏼‍💻</text></svg>"'
+            }
+          ]
+        }
         openGraph={
           {
             url: 'https://tylangesmith.com/',
