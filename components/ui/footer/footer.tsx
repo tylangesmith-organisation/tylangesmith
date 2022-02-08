cb464226d9ee0161cdd28aa36fc0d9e01caa4409
Ty Lange-Smith <tylangesmith@gmail.com>
2022-02-08 21:04:00 +1100

    Add a way to get back to the home page from footer

--- components/ui/footer/footer.tsx
+++ components/ui/footer/footer.tsx
@@ -4,14 +4,15 @@ import Container from '@/components/ui/container/container'
 interface FooterItemProps {
   href: string
   text: string
+  blank?: boolean
 }
 
 const FooterItem = (props: FooterItemProps) => {
-  const { href, text } = props
+  const { href, text, blank = true } = props
   return (
     <li className="mb-2 sm:mb-0 sm:mr-4">
       <Link href={href}>
-        <a className="font-black text-xl" target="_blank">{text}</a>
+        <a className="font-black text-xl" target={blank ? '_blank' : ''}>{text}</a>
       </Link>
     </li>
   )
@@ -22,6 +23,7 @@ const Component = () => {
     <footer className="bg-gray-100 py-12">
       <Container>
         <ul className="flex flex-col items-center sm:flex-row">
+          <FooterItem href="/" text="tylangesmith.com" blank={false} />
           <FooterItem href="https://github.com/tylangesmith/" text="GitHub" />
           <FooterItem href="https://www.linkedin.com/in/tylangesmith/" text="LinkedIn" />
         </ul>
