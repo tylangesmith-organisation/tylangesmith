299b14c0c89f529a01c45bc1b10aec085da71869
Ty Lange-Smith <tylangesmith@gmail.com>
2022-02-08 20:20:58 +1100

    Add Implementation Section (#23)
    * Add `gist` component and start implementation section
    
    * Update AWS CDK package versions
    
    * Update stack env vars
    
    * Update blog name
    
    * Update snapshot tests
    
    * Update CDK issue
    
    * Content updates
    
    * Content updates
    
    * Reword intro
    
    * Content updates
    
    * Content and diagram updates
    
    * Add some better image styling
    
    * Add AWS CDK introduction section
    
    * Add creating CloudFormation stack gist
    
    * More content updates
    
    * More content updates
    
    * Lots more content updates
    
    * More content updates
    
    * Content updates
    
    * Content updates
    
    * More content updates
    
    * Update `Creating a Bucket` section
    
    * More content updates

--- components/pages/blog/slug/index.tsx
+++ components/pages/blog/slug/index.tsx
@@ -1,4 +1,5 @@
 import { MDXRemote } from 'next-mdx-remote'
+import Gist from 'react-gist'
 
 import PostLayout from '@/components/layout/postLayout'
 import Section from '@/components/ui/container/section'
@@ -6,6 +7,7 @@ import { Props } from '@/components/pages/blog/slug/controller'
 
 import A from '@/components/ui/typography/a'
 import Image from '@/components/ui/image/image'
+import ProseWidth from '@/components/ui/container/proseWidth'
 
 const components = {
   Section: (props: any) => <Section {...props} />,
@@ -13,9 +15,11 @@ const components = {
   h3: (props: any) => <h3 className="my-2" {...props} />,
   p: (props: any) => <p className="my-2 text-lg font-normal" {...props} />,
   a: (props: any) => <A {...props} />,
-  blockquote: (props: any) => <blockquote className="border-blue-600 bg-gray-50" {...props} />,
+  blockquote: (props: any) => <blockquote className="border-blue-600 bg-gray-100" {...props} />,
   inlineCode: (props: any) => <code className="px-2 py-1 font-normal rounded-sm bg-gray-200" {...props} />,
-  Image: (props: any) => <Image {...props} />
+  Image: (props: any) => <Image {...props} />,
+  Gist: (props: any) => <Gist {...props} />,
+  ProseWidth: (props: any) => <ProseWidth {...props} />
 }
 
 const Component = (props: Props): JSX.Element => {
